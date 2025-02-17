244c98b67b3c65b50cf5f4428d2977bc8d01fd70
nacycher <nacycher@163.com>
2025-02-17 11:00:07 +0800

    init project

---
+++ src/main/java/org/lb/studyinterview/StudyInterviewApplication.java
@@ -0,0 +1,13 @@
+package org.lb.studyinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lb/studyinterview/StudyInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lb.studyinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
